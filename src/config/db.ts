0658d38c430ad37b11d2fff99c90083aaaad3b19
Sidney Sousa <sidney.sousa@ifms.edu.br>
2021-06-09 18:16:14 -0400

    minor corrections

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import { createConnection } from 'typeorm'
 
 export const connectToDB = async () => {
     const conn = await createConnection()
-    console.log(`App connected do db ${conn.options.database}`)
+    console.log(`App connected to db ${conn.options.database}`)
 
     process.on('SIGINT', () => {
         conn.close().then(() => console.log('DB connection closed'))
